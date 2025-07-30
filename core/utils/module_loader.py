5fd6e144dbf44409617b12fc84dfb5f0c9b825aa
Farrid Putra <farridputra@gmail.com>
2025-07-30 12:27:24 +0700

    improved module_loader process

--- core/utils/module_loader.py
+++ core/utils/module_loader.py
@@ -14,6 +14,29 @@ def get_installed_modules():
     return ModuleRegistry.objects.filter(is_installed=True).values_list('slug', flat=True)
 
 
+def run_management_command(*args):
+    """
+    Running the Django manage.py command.
+    Example: run_management_command('migrate', 'app_name')
+
+    Returns:
+        True if successful (exit code 0), False if unsuccessful.
+    """
+    print(f"\n🚀 Running: python manage.py {' '.join(args)}")
+    result = subprocess.run(['python', 'manage.py', *args, '--noinput'], capture_output=True, text=True)
+
+    if result.returncode == 0:
+        print("Command succeeded.")
+        return True, result.stdout.strip()
+    else:
+        print("Command failed.")
+        warning = result.stdout.strip()
+        error = result.stderr.strip()
+        if 'non-nullable field' in warning and 'without specifying a default' in warning:
+            return False, warning
+        return False, error
+
+
 def install_module(slug):
     try:
         module = ModuleRegistry.objects.get(slug=slug)
@@ -23,9 +46,14 @@ def install_module(slug):
     if module.is_installed:
         return False  # already installed
 
-    # Jalankan makemigrations dan migrate untuk module tersebut
-    subprocess.call(['python', 'manage.py', 'makemigrations', slug])
-    subprocess.call(['python', 'manage.py', 'migrate', slug])
+    # Run makemigrations and migrate for module
+    success, message = run_management_command('makemigrations', slug)
+
+    if success:
+        run_management_command('migrate', slug)
+    else:
+        # Return Failed
+        raise ValueError(message)
 
     module.is_installed = True
     module.save()
@@ -41,26 +69,33 @@ def uninstall_module(slug):
     module.is_installed = False
     module.save()
 
-    # tabel data tetap ada — hanya dinonaktifkan
+    # data in table still exists — only disabled
     return module, True
 
 
 def upgrade_module(slug):
     try:
         module = ModuleRegistry.objects.get(slug=slug)
-        # Update version
-        old_version = module.version
-        major, minor, patch = map(int, old_version.split('.'))
-        module.version = f"{major}.{minor}.{patch + 1}"
 
-        module.save()
     except ModuleRegistry.DoesNotExist:
         raise ValueError(f"Module with slug '{slug}' is not registered")
 
     if not module.is_installed:
         raise ValueError("Module must be installed before upgrading")
 
-    subprocess.call(['python', 'manage.py', 'makemigrations', slug])
-    subprocess.call(['python', 'manage.py', 'migrate', slug])
+    success, message = run_management_command('makemigrations', slug)
+
+    if success:
+        run_management_command('migrate', slug)
+
+        # Update version
+        old_version = module.version
+        major, minor, patch = map(int, old_version.split('.'))
+        module.version = f"{major}.{minor}.{patch + 1}"
+
+        module.save()
+    else:
+        # Return Failed
+        raise ValueError(message)
 
     return module, True
